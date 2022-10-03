2bce1e3326d442d1d21922c797e870cda1baf15c
calimendoza <mbaivictor1@gmail.com>
2022-10-03 02:48:16 -0700

    added state to the task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
+
+
 const App = () => {
+      const[tasks, setTasks]   =useState(
+  [
+    {
+      "id": 1,
+      "text": "test",
+      "other": "no-show",
+      "day": "monday",
+      "reminder": true
+    },
+    {
+      "text": "Test4",
+      "day": "Wednesday",
+      "reminder": false,
+      "id": 3
+    },
+    {
+      "text": "Task6",
+      "day": "Thursday",
+      "reminder": true,
+      "id": 4
+    }
+  ]
+  )
 
    return (
     <div className="container">
       <Header  />
-      <Tasks />
+      <Tasks  tasks={tasks}/>
     </div>
   );
 }
