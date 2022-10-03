737100c3528a7c174d33ce8ec0d4f701f125a9d8
calimendoza <mbaivictor1@gmail.com>
2022-10-03 08:05:29 -0700

    add delete task

--- src/App.js
+++ src/App.js
@@ -28,10 +28,15 @@ const App = () => {
   ]
   )
 
+
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
    return (
     <div className="container">
       <Header  />
-      <Tasks  tasks={tasks}/>
+      <Tasks  tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
